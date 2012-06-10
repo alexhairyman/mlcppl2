6d08397085390e43a93f7c9257f91ae26a8506bf
alexhairyman <alexhairyman@gmail.com>
2012-06-10 16:37:26 -0600

    initial commit, header file is basically all done and complete as I see right now. Will almost definitely change in the future. done:
    * build system
    * function pointers for read/write open
    
    started:
    
    * arcfile

---
+++ libmlcppl/include/arcy.hh
@@ -0,0 +1,164 @@
+/*
+Copyright (c) <2012> alex herrmann (alexhairyman@gmail.com)
+
+Permission is hereby granted, free of charge, to any person 
+obtaining a copy of this software and associated documentation 
+files (the "Software"), to deal in the Software without 
+restriction, including without limitation the rights to use, copy, 
+modify, merge, publish, distribute, sublicense, and/or sell copies 
+of the Software, and to permit persons to whom the Software is 
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be 
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS 
+BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+SOFTWARE.
+*/
+
+#pragma once
+// libarchive includes
+#include <archive.h>
+#include <archive_entry.h>
+
+// Standard Library include
+#include <string>
+#include <iostream>
+#include <sstream>
+
+// STL container includes
+// Do I really need map?
+#include <vector>
+#include <list>
+#include <deque>
+
+using namespace std;
+
+namespace ml
+{
+  
+  // mode for the archive
+  enum MODE
+  {
+    M_NONE=0,
+    M_READ=1,
+    M_WRITE=2
+  };
+
+  // type of archive, and it's callbacks
+  enum ARC_T
+  {
+    ARCT_NONE=0,
+    ARCT_ZIP=1,
+    ARCT_PAX=2,
+    ARCT_USTAR=3,
+    ARCT_7Z=4,
+    ARCT_ISO=5
+  };
+  // vectors of function pointers
+  typedef int (*arc_fmt_ptr)(struct archive*);
+  // typedef int (*readptr)(struct archive*, const char*, int);
+  
+  
+  arc_fmt_ptr __delegate_correct_read_ptr(ARC_T atype);
+  arc_fmt_ptr __delegate_correct_write_ptr(ARC_T atype);
+  
+  arc_fmt_ptr get_correct_arcptr(ARC_T atype, MODE _mode);
+  
+  // Not really used for anything yet
+  enum COMP_T
+  {
+    COMPT_NONE=0,
+    COMPT_GZIP=1,
+    COMPT_BZIP2=2,
+    COMPT_LZMA=3
+  };
+  
+  class arcent
+  {
+  public:
+    // archive_entry stuff
+    arcent();
+    
+    void setpath(string path);
+    string getpath();
+    
+    void setsize(int size);
+    int getsize();
+    
+    void setperms(string perms);
+    string getperms();
+    
+    void setbtime(int time);
+    int getbtime();
+    
+    void setmtime(int time);
+    int getmtime();
+    
+    void setctime(int time);
+    int getctime();
+    
+    void addchar(char x);
+    void write(string data);
+    void write(char * data);
+    void write (const char * data);
+    
+    archive_entry getarcent();
+    
+  private:
+  
+    stringbuf* _mdata;
+    iostream* _dataw;
+    
+    string _path;
+    bool _path_set;
+    
+    string _perms;
+    
+    int _size;
+    bool _size_set;
+    
+    int _mtime;
+    int _btime;
+    int _ctime;
+    
+    struct archive_entry* _archive_entry;
+  };
+  
+  /*
+   * an archive file
+   * you must specify a mode because an archive can only read or write,
+   * not both though
+   */
+   
+  /*
+   * Maybe in the future I will add a way to switch them so you can
+   * change modes
+   */
+  class arcfile
+  {
+  public:
+    arcfile(MODE mode);
+    ~arcfile();
+    
+    void addentry(arcent entry);
+    arcent readentry(int location);
+    
+    void setfilename(string filename);
+    
+  private:
+    struct archive* _archive;
+    
+    bool isinitialized;
+    bool noerror;
+    
+    MODE _mode;
+    
+  };
+}
---
+++ libmlcppl/src/arcy_util.cc
@@ -0,0 +1,67 @@
+/*
+Copyright (c) <2012> alex herrmann (alexhairyman@gmail.com)
+
+Permission is hereby granted, free of charge, to any person 
+obtaining a copy of this software and associated documentation 
+files (the "Software"), to deal in the Software without 
+restriction, including without limitation the rights to use, copy, 
+modify, merge, publish, distribute, sublicense, and/or sell copies 
+of the Software, and to permit persons to whom the Software is 
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be 
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS 
+BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+SOFTWARE.
+*/
+#include "arcy.hh"
+
+namespace ml
+{
+  arc_fmt_ptr __delegate_correct_read_ptr(ARC_T atype)
+  {
+    arc_fmt_ptr pta;
+    if (atype == ARCT_7Z)
+      pta = &archive_read_support_format_7zip;
+    else if (atype == ARCT_ISO)
+      pta = &archive_read_support_format_iso9660;
+    else if (atype == ARCT_PAX || atype == ARCT_USTAR)
+      pta = &archive_read_support_format_tar;
+    else if (atype == ARCT_ZIP)
+      pta = &archive_read_support_format_zip;
+      
+    return pta;
+  }
+
+  arc_fmt_ptr __delegate_correct_write_ptr(ARC_T atype)
+  {
+    arc_fmt_ptr pta;
+    if (atype == ARCT_7Z)
+      pta = &archive_write_set_format_7zip;
+    else if (atype == ARCT_ISO)
+      pta = &archive_write_set_format_iso9660;
+    else if (atype == ARCT_PAX)
+      pta = &archive_write_set_format_pax_restricted;
+    else if (atype == ARCT_USTAR)
+      pta = &archive_write_set_format_ustar;
+    else if (atype == ARCT_ZIP)
+      pta = &archive_write_set_format_ustar;
+    
+    return pta;
+  }
+
+  arc_fmt_ptr get_correct_arcptr(ARC_T atype, MODE _mode)
+  {
+    if (_mode == M_READ)
+      return __delegate_correct_read_ptr(atype);
+    else if (_mode == M_WRITE)
+      return __delegate_correct_write_ptr(atype);
+  }
+}
